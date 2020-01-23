36796c9125b30ee7784bb0bce3143d1d214a9acd
becover <sedi.september@gmail.com>
2020-01-23 12:10:45 +0900

    calendar shown/hide 기능 구현

--- src/components/revervation/Form.js
+++ src/components/revervation/Form.js
@@ -1,29 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Calendar from "react-calendar";
 import icon_home from "../../asset/img/icon_home.png";
 
 const Form = () => {
-  const [date, setDate] = useState(new Date());
-  const [inputValue, setInputValue] = useState("");
+  const [date, setDate] = useState(new Date()); // 달력에 보낼 날짜 값
+  const [inputValue, setInputValue] = useState(""); // 인풋창에 보낼 날짜 값
   const [toggleValue, setToggleValue] = useState({
+    // 달력을 on/off하기 위한 상태 값
     isClicked: false
   });
-  console.log(inputValue);
+  const inputRef = useRef();
+
+  //선택한 날짜 포매팅
   const formatDate = date => {
+    //오늘 날짜
     const today = new Date();
     const thisYear = today.getFullYear();
     let thisMonth = today.getMonth() + 1;
     thisMonth = thisMonth >= 10 ? thisMonth : `0${thisMonth}`;
     let thisDay = today.getDate();
     thisDay = thisDay >= 10 ? thisDay : `0${thisDay}`;
-
+    //선택한 날짜
     const year = date.getFullYear();
     let month = date.getMonth() + 1;
     month = month >= 10 ? month : `0${month}`;
     let day = date.getDate();
     day = day >= 10 ? day : `0${day}`;
-
+    //과거일을 선택하는지 체크
     if (
       parseInt(`${year}${month}${day}`, 10) -
         parseInt(`${thisYear}${thisMonth}${thisDay}`, 10) <
@@ -32,23 +36,37 @@ const Form = () => {
       alert("과거일은 예약날짜로 선택할 수 없습니다");
       return "";
     }
+    //문제없다면 선택한 날짜를 YYYY-MM-DD 포맷으로 반환
     return `${year}-${month}-${day}`;
   };
+
   const onDateChange = date => {
     setDate(formatDate(date));
   };
-  const onValueChange = date => {
-    setInputValue(formatDate(date));
-    setToggleValue({
-      ...toggleValue,
-      isClicked: !toggleValue.isClicked
-    });
-  };
+
   const onToggleValue = () =>
     setToggleValue({
       ...toggleValue,
       isClicked: !toggleValue.isClicked
     });
+
+  const onValueChange = date => {
+    setInputValue(formatDate(date));
+    onToggleValue();
+  };
+
+  const handleClick = e => {
+    if (toggleValue.isClicked && !inputRef.current) {
+      onToggleValue();
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("click", handleClick);
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  });
+
   return (
     <>
       <div className="sub_container">
@@ -63,7 +81,6 @@ const Form = () => {
 
         <div className="sub_cont">
           <h1 className="title_style_2">예약하기</h1>
-
           <div className="estimate_wrap">
             <form action="#" method="post">
               <ul className="clfx">
@@ -87,6 +104,7 @@ const Form = () => {
                     value={inputValue}
                     readOnly
                     onClick={onToggleValue}
+                    ret={inputRef}
                   />
                   {toggleValue.isClicked && (
                     <Calendar
