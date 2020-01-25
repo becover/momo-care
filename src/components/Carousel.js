0610cc8168dbfa251a02608de357b88133907dbe
becover <sedi.september@gmail.com>
2020-01-25 22:42:44 +0900

    add component slider carousel

---
+++ src/components/Carousel.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect, cloneElement } from "react";
+
+const Carousel = props => {
+  console.log(props);
+  const [active, setActive] = useState(0);
+  let scrollInterval = null;
+  const style = {
+    carousel: {
+      position: "relative"
+    },
+    carouselItem: {
+      position: "absolute",
+      visibility: "hidden"
+    },
+    visible: {
+      visibility: "visible"
+    }
+  };
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    scrollInterval = setTimeout(() => {
+      const { carouselItems } = props;
+      setActive((active + 1) % carouselItems.length);
+    }, 50000);
+    return () => clearTimeout(scrollInterval);
+  });
+  const { carouselItems, ...rest } = props;
+  return (
+    <div style={style.carousel}>
+      {carouselItems.map((item, index) => {
+        const activeStyle = active === index ? style.visible : {};
+        return cloneElement(item, {
+          ...rest,
+          style: {
+            ...style.carouselItem,
+            ...activeStyle
+          }
+        });
+      })}
+    </div>
+  );
+};
+
+export default Carousel;
